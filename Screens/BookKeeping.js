4424319f030c149d9cb70539161e2d2f4de0c486
Qais Nasir <qaisnasir22@gmail.com>
2022-04-13 11:28:12 +0500

    bookKeeping completed according to design

--- Screens/BookKeeping.js
+++ Screens/BookKeeping.js
@@ -1,18 +1,41 @@
 import { StyleSheet, Text, View } from "react-native";
-import React,{useEffect} from "react";
+import React,{useState} from "react";
 import Drawer from "../Src/Components/Drawer";
 import { useData } from "../Src/Hooks/useData";
-import { SafeAreaView } from "react-native-safe-area-context";
+import TabBar from "../Src/Components/TabBar";
 
 export default function BookKeeping() {
-  const  {color,setColor} = useData()
+  const data = ["Products", "Inventry", "Manufactur", "Reports"];
+  const [indexofitem, setIndexofitem] = useState(0);
+  const {setColor,color} = useData()
+  const colors = ["#ef3365", "#26985d", "#1c98be", "yellow"];
   return (
-      <View style={styles.container}>
-        <Drawer />
-        <Text style={styles.text}>
-          This is BookKeeping Screen
-        </Text>
-      </View>
+    <View style={{  flexDirection:"row", flex:1, backgroundColor:"black" }}>
+        
+    <View style={{alignSelf:"center"}}>
+      <Drawer  />
+    </View> 
+
+<View>
+  
+  <TabBar
+    txt={data}
+    indexofitem={indexofitem}
+    setIndexofitem={setIndexofitem}
+    colors={colors}
+  />
+  {indexofitem === 0 ? (
+      <Text style={styles.text}>Customers Component here</Text>
+  ) : indexofitem === 1 ? (
+    <Text style={styles.text}>Potentials Component here</Text>
+  ) : indexofitem === 2 ? (
+    <Text style={styles.text}>Leads Component here</Text>
+  ) : indexofitem === 3 ? (
+    <Text style={styles.text}>Reports Component here</Text>
+  ) : null}
+
+  </View>    
+</View>
   );
 }
 
